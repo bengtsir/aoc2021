1192186fc5fbb6b403f0df6ef5610ccd337ee20f
bengto <bengto@axis.com>
2021-12-03 11:47:07 +0100

    Day 2 done

---
+++ aoc2021/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Day2().Part1();
+            new Day2().Part2();
+        }
+    }
+}
