aee07e08ef4c04b5e1aaed21216a70c03618aa66
Bengt Sirbelius <bengto@axis.com>
2021-12-03 15:44:13 +0100

    Day 3 complete

--- aoc2021/Program.cs
+++ aoc2021/Program.cs
@@ -10,8 +10,8 @@ namespace aoc2021
     {
         static void Main(string[] args)
         {
-            new Day2().Part1();
-            new Day2().Part2();
+            new Day3().Part1();
+            new Day3().Part2();
         }
     }
 }
