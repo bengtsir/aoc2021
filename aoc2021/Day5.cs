98ddb55e3492b1b0c7c5f44eef444e134241b063
Bengt Sirbelius <bengto@axis.com>
2021-12-06 14:09:05 +0100

    Day 6 done

--- aoc2021/Day5.cs
+++ aoc2021/Day5.cs
@@ -55,5 +55,66 @@ namespace aoc2021
 
             Console.WriteLine($"Number of overlaps: {count}");
         }
+
+
+        public void Part2()
+        {
+            var data = File.ReadAllLines(@"data\day5.txt");
+
+            var grid = new int[1000, 1000];
+
+            var sep = new char[] { ' ', ',', '-', '>' };
+            var opt = StringSplitOptions.RemoveEmptyEntries;
+
+            foreach (var line in data)
+            {
+                var n = line.Split(sep, opt).Select(x => Int32.Parse(x)).ToArray();
+
+                if (n[0] == n[2])
+                {
+                    for (int i = Math.Min(n[1], n[3]); i <= Math.Max(n[1], n[3]); i++)
+                    {
+                        grid[n[0], i]++;
+                    }
+                }
+                else if (n[1] == n[3])
+                {
+                    for (int i = Math.Min(n[0], n[2]); i <= Math.Max(n[0], n[2]); i++)
+                    {
+                        grid[i, n[1]]++;
+                    }
+                }
+                else
+                {
+                    var xstep = n[2] > n[0] ? 1 : -1;
+                    var ystep = n[3] > n[1] ? 1 : -1;
+                    var ofs = 0;
+
+                    while (n[0] + ofs*xstep != n[2])
+                    {
+                        grid[n[0] + ofs * xstep, n[1] + ofs * ystep]++;
+                        ofs++;
+                    }
+                    grid[n[2], n[3]]++;
+                }
+            }
+
+            var count = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    if (grid[i, j] > 1)
+                    {
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Number of overlaps: {count}");
+        }
     }
 }
---
+++ aoc2021/Day6.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2021
+{
+    internal class Day6
+    {
+        public void Part1()
+        {
+            var data = File.ReadAllLines(@"data\day6.txt");
+
+            var fish = new int[]
+            {
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+            };
+
+            foreach (var idx in data[0].Split(',').Select(x => Int32.Parse(x)))
+            {
+                fish[idx]++;
+            }
+
+            Console.WriteLine($"Initial fish: {fish.Sum()}");
+
+            for (int i = 0; i < 80; i++)
+            {
+                var zeros = fish[0];
+                fish = fish.Skip(1).Append(zeros).ToArray();
+                fish[6] += zeros;
+
+                Console.WriteLine($"Day {i + 1}: {fish.Sum()}");
+            }
+        }
+
+        public void Part2()
+        {
+            var data = File.ReadAllLines(@"data\day6.txt");
+
+            var fish = new long[]
+            {
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+            };
+
+            foreach (var idx in data[0].Split(',').Select(x => Int32.Parse(x)))
+            {
+                fish[idx]++;
+            }
+
+            Console.WriteLine($"Initial fish: {fish.Sum()}");
+
+            for (int i = 0; i < 256; i++)
+            {
+                var zeros = fish[0];
+                fish = fish.Skip(1).Append(zeros).ToArray();
+                fish[6] += zeros;
+
+                Console.WriteLine($"Day {i + 1}: {fish.Sum()}");
+            }
+        }
+    }
+}
